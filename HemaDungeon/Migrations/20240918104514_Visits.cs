2ad646e9fbe86d770bbdcf68ccf7080ad8a9719c
Bogdan Malcev <bogdan-malcev@yandex.ru>
2024-09-18 17:47:11 +0700

    database

---
+++ HemaDungeon/Migrations/20240918104514_Visits.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HemaDungeon.Migrations
+{
+    /// <inheritdoc />
+    public partial class Visits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Visit_AspNetUsers_CharacterId",
+                table: "Visit");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Visit",
+                table: "Visit");
+
+            migrationBuilder.RenameTable(
+                name: "Visit",
+                newName: "Visits");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Visit_CharacterId",
+                table: "Visits",
+                newName: "IX_Visits_CharacterId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Visits",
+                table: "Visits",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Visits_AspNetUsers_CharacterId",
+                table: "Visits",
+                column: "CharacterId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Visits_AspNetUsers_CharacterId",
+                table: "Visits");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Visits",
+                table: "Visits");
+
+            migrationBuilder.RenameTable(
+                name: "Visits",
+                newName: "Visit");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Visits_CharacterId",
+                table: "Visit",
+                newName: "IX_Visit_CharacterId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Visit",
+                table: "Visit",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Visit_AspNetUsers_CharacterId",
+                table: "Visit",
+                column: "CharacterId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
