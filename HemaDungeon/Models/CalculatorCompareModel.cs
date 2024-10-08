dcfd2daf65bdddb58686690130ef0da30dfef7a6
Bogdan Malcev <bogdan-malcev@yandex.ru>
2024-10-08 18:19:41 +0700

    First

---
+++ HemaDungeon/Models/CalculatorCompareModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HemaDungeon.Models;
+
+public sealed class CalculatorCompareModel
+{
+    public User FirstUser { get; set; }
+
+    public User SecondUser { get; set; }
+ 
+    public sealed record User(string Id, int? Health, int? Score, int? Damage);
+}
