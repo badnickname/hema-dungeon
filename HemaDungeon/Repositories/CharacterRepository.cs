5e4605ce062c16cf8d760ab9ab346fa749af93c9
Bogdan Malcev <bogdan-malcev@yandex.ru>
2024-10-11 23:56:54 +0700

    Pages

--- HemaDungeon/Repositories/CharacterRepository.cs
+++ HemaDungeon/Repositories/CharacterRepository.cs
@@ -7,7 +7,7 @@ public sealed class CharacterRepository(Context context)
 {
     public async Task<ICollection<Character>> GetAllCharacters()
     {
-        var characters = context.Users.Include(x => x.Visits).Include(x => x.Cataclysms);
+        var characters = context.Users.Include(x => x.Visits).Include(x => x.Cataclysms).Include(x => x.Pages);
         return await characters.ToListAsync(); 
     }
 }
