b2ab6f7e70d712de93181b431cef5b90f2a47724
Bogdan Malcev <bogdan-malcev@yandex.ru>
2024-10-18 14:04:26 +0700

    Deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/bash
+https=$1
+username=$2
+password=$3
+stack=$4
+endpoint=$5
+
+JWT=$(curl -s -X POST ${https}/api/auth -H "Content-Type: application/json" -d "{\"username\": \"${username}\", \"password\": \"${password}\"}" | jq '.jwt' -r)
+
+CONTENT=$(curl -s -X GET ${https}/api/stacks/${stack}/file -H "Content-Type: application/json" -H "Authorization: Bearer ${JWT}"  | jq '.StackFileContent' -r -a | tr -d '"')
+echo "Content: "
+echo "$CONTENT"
+
+RESULT=$(curl -s -X PUT ${https}/api/stacks/${stack}?endpointId=${endpoint} -H "Content-Type: application/json" -H "Authorization: Bearer ${JWT}" -d "{\"id\":${stack},\"StackFileContent\":\"${CONTENT}\",\"Env\":[],\"Prune\":false,\"PullImage\":true}" | jq)
+echo "Result: "
+echo "$RESULT"
