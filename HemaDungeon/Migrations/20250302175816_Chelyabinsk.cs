aa2c82eb1ef0d28f1ff3979f1d569406bfb06d48
Bogdan Malcev <bogdan-malcev@yandex.ru>
2025-03-03 00:59:01 +0700

    Migration

---
+++ HemaDungeon/Migrations/20250302175816_Chelyabinsk.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HemaDungeon.Migrations
+{
+    /// <inheritdoc />
+    public partial class Chelyabinsk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO \"Regions\" (\"Id\", \"Name\") VALUES ('CHELYABA', 'Челябинск')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
